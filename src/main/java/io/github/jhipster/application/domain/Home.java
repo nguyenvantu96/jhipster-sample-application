1ff011ef9d91fec49cd9c55927344796fde9b8ca
nguyentu13 <tu.nguyen@solidtech.vn>
2019-05-02 10:31:09 +0700

    commit code

---
+++ src/main/java/io/github/jhipster/application/domain/Home.java
@@ -0,0 +1,38 @@
+package io.github.jhipster.application.domain;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "home")
+public class Home {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name= "name")
+    private String name;
+
+    public Home() {
+    }
+
+    public Home(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
